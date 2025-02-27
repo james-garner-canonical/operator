a305e4266e405f5fde1a098e6c46e260d2aa6378
James Garner <james.garner@canonical.com>
2025-02-28 10:46:00 +1300

    feat: exit replace early if no changes/deletes are required

---
+++ test/integration/test_relations.py
@@ -0,0 +1,43 @@
+from __future__ import annotations
+
+import json
+
+import jubilant
+import pytest
+
+CHARM = 'basic'
+MODEL = f'{CHARM}-model'
+UNIT = f'{CHARM}/0'
+
+@pytest.fixture(scope='module')
+def juju():
+    j = jubilant.Juju()
+    j.add_model(MODEL)
+    try:
+        j.deploy(
+            './test/integration/basic_charm/basic_ubuntu-22.04-amd64.charm',
+            resource={'demo-server-image': 'ubuntu:latest'},
+        )
+        yield j
+    finally:
+        j.destroy_model(MODEL)
+
+
+def get_relation_data(juju: jubilant.Juju) -> dict[str, str]:
+    args = ('run', UNIT, 'basic-action', '--format=json')
+    return json.loads(json.loads(juju.cli(*args))[UNIT]['results']['get'].replace("'", '"'))
+
+
+def set_relation_data(juju: jubilant.Juju, data: dict[str, str]) -> dict[str, str]:
+    args = ('run', UNIT, 'basic-action', '--format=json', f'data=\'{json.dumps(data)}\'')
+    return json.loads(json.loads(juju.cli(*args))[UNIT]['results']['set'].replace("'", '"'))
+
+
+def test_assign_relation_data(juju: jubilant.Juju):
+    original = get_relation_data(juju)
+    print(original)
+    assert set(original) == {'egress-subnets', 'ingress-address', 'private-address'}
+    data = {'foo': 'bar', 'baz': 'bartholemew'}
+    new = set_relation_data(juju, data)
+    print(new)
+    assert new == data
