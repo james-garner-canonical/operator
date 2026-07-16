808cc43c142aab19b4ed11292056ab779b3ee417
James Garner <james.garner@canonical.com>
2026-07-16 19:36:11 +1200

    ci: don't use -r

--- .scripts/pre-commit-hooks-format.sh
+++ .scripts/pre-commit-hooks-format.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 set -xueo pipefail
 cd "$(git rev-parse --show-toplevel)"
+
 NONBINARY_FILES=$(git grep -Il '' -- .)
 
 set -x  # Print commands as they are executed.
-xargs -r end-of-file-fixer <<< "$NONBINARY_FILES"
-xargs -r mixed-line-ending <<< "$NONBINARY_FILES"
-xargs -r trailing-whitespace-fixer <<< "$NONBINARY_FILES"
+xargs end-of-file-fixer <<< "$NONBINARY_FILES"
+xargs mixed-line-ending <<< "$NONBINARY_FILES"
+xargs trailing-whitespace-fixer <<< "$NONBINARY_FILES"
